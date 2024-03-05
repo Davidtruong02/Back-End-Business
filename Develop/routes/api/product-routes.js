24cfe259a8fb4ee27e29d3cc0a5c2675c017a65a
David Truong <Davidtruong134@gmail.com>
2024-03-04 20:42:05 -0600

    final commit :)

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -33,7 +33,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-  
+
 });
 
 // create new product
@@ -115,6 +115,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((deletedProduct) => {
+    if (!deletedProduct) {
+      res.status(404).json({ message: 'No product found with this id' });
+      return;
+    }
+    res.json(deletedProduct);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
